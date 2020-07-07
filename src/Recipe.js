8c285467ddbdf6d723d5afe745a9e3b415bd3a3a
Dawid Rynkal <dawidos14051997@gmail.com>
2020-07-07 18:17:29 +0200

    Recipe app with using EDAMAM API

---
+++ src/Recipe.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import style from './recipe.module.css'
+
+const Recipe = ({ title, calories, image, ingredients }) => {
+
+    return (
+        <div className={style.recipe}>
+            <h1>{title}</h1>
+            <p className={style.kcl}>{Math.round(calories)} kcl</p>
+            <img className={style.image} src={image} alt="chicken" />
+            <ol>
+                {ingredients.map(ingredient => (
+                    <li>
+                        {ingredient.text}
+                    </li>
+                )
+                )}
+            </ol>
+
+        </div>
+    )
+}
+
+export default Recipe;
